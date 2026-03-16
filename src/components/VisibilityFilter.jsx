005e5cfa13137e40b0f0045035f3e591e093d784
conveyedbee <conveyedbee@mailbox.org>
2026-03-16 16:19:07 +0700

    many reducers, one store

---
+++ src/components/VisibilityFilter.jsx
@@ -0,0 +1,16 @@
+import { useDispatch } from 'react-redux'
+import { filterChange } from '../reducers/filterReducer'
+
+const VisibilityFilter = () => {
+    const dispatch = useDispatch()
+
+    return (
+        <div>
+            <input type='radio' name='filter' onChange={() => dispatch(filterChange('ALL'))}/>all
+            <input type='radio' name='filter' onChange={() => dispatch(filterChange('IMPORTANT'))}/>important
+            <input type='radio' name='filter' onChange={() => dispatch(filterChange('NONIMPORTANT'))}/>not important
+        </div>
+    )
+}
+
+export default VisibilityFilter
